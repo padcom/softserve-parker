7dc454f6208904700a386ba55a2d6b98e15b13a8
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-08-02 16:09:50 +0200

    Cleanup of LoginForm test component

--- frontend/tests/e2e/components/LoginForm.ts
+++ frontend/tests/e2e/components/LoginForm.ts
@@ -1,9 +1,21 @@
 import Component from './Component'
 
 export default class LoginForm extends Component {
+  get username () {
+    return this.root.find('[name="username"]')
+  }
+
+  get password () {
+    return this.root.find('[name="password"]')
+  }
+
+  get submit () {
+    return this.root.find('[name="login"]')
+  }
+
   login (username: string, password: string) {
-    this.root.find('[name="username"]').clear().type(username)
-    this.root.find('[name="password"]').clear().type(password)
-    this.root.find('[name="login"]').click()
+    this.username.clear().type(username)
+    this.password.clear().type(password)
+    this.submit.click()
   }
 }
