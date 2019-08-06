f7afbc8d0572a9467497ece14a850677057454d2
Artur Tagisow <atagi@softserveinc.com>
2019-08-06 17:42:37 +0200

    Fix 'no-trailing-spaces' rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    browser: true
+    browser: true,
   },
   extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     indent: ['error', 2],
-    'no-trailing-space': ['error'],
-    'eol-last': ['error', 'always']
+    'no-trailing-spaces': ['error'],
+    'eol-last': ['error', 'always'],
   },
   parserOptions: {
     parser: 'babel-eslint',
-    rules: {}
-  }
-}
+    rules: {},
+  },
+};
