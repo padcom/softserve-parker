425cb5e0872742b2f193f42c9ebadcccd46c9fe5
Artur Tagisow <atagi@softserveinc.com>
2019-08-06 17:36:26 +0200

    Removed .editorconfig and moved it's rules to .eslintrc.js
    People can just use the VSCode option "autofix all autofixable problems" to adhere to these rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    indent: ['error', 2],
+    'no-trailing-space': ['error'],
+    'eol-last': ['error', 'always']
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    rules: {}
   }
 }
