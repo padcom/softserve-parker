9dd00610a2a7595cf652184e1620b6a74c613c83
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-09-17 12:01:45 +0200

    Code cleanup

--- backend/app/graphql/parkingspots/typedefs.ts
+++ backend/app/graphql/parkingspots/typedefs.ts
@@ -2,8 +2,7 @@ import { Arg, Field, ID, Int, ObjectType, Query, Resolver } from 'type-graphql'
 import { ParkingSpotService } from './service'
 
 @ObjectType({
-  // In normal graphql, this would be type ParkingSpots { id: ID!, userId: Int }
-  description: 'Returns all parking spots. THIS WILL APPEAR IN THE DOCUMENTATION EXPLORER IN APOLLOGRAPHQL. Click "Docs" on the right on localhost:4000',
+  description: 'Returns all parking spots.',
 })
 export class ParkingSpots {
   @Field(() => ID)
@@ -19,15 +18,16 @@ export class ParkingSpots {
 @Resolver(ParkingSpots)
 export class ParkingSpotsResolver {
   @Query(() => [ ParkingSpots ], {
-    description: 'Query description. Returns all parking spots up to limit.',
+    description: 'Returns all parking spots up to limit.',
   })
   async parkingspots(
     @Arg('skip', () => Int!, {
-      description: 'Method argument description. This is argument defines how many rows to skip',
+      description: 'This is argument defines how many rows to skip',
     })
     skip: number,
+
     @Arg('limit', () => Int!, {
-      description: 'Method argument description. This is argument limits the amount of rows returned',
+      description: 'This is argument limits the amount of rows returned',
     })
     limit: number
   )
