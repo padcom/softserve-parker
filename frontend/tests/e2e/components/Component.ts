72c5874d1b7fece2f2c276d3b72617a71eacb2ba
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-09-17 09:28:06 +0200

    Fixed Cypress Component to properly work with the login form

--- frontend/tests/e2e/components/Component.ts
+++ frontend/tests/e2e/components/Component.ts
@@ -1,4 +1,8 @@
 export default class Component {
+  constructor (private _root: JQuery<any>) {
+    this.root.should('exist')
+  }
+
   get root (): Cypress.Chainable<JQuery<any>> {
     return cy.wrap(this._root)
   }
