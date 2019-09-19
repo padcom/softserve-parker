b483420301c29088f833c27dd3ee7cf5c8a1570a
wtart-softserve <wtart@softserveinc.com>
2019-09-19 15:42:58 +0200

    SSPAR-2 Added basic authentication system.

--- backend/app/main.ts
+++ backend/app/main.ts
@@ -1,10 +1,10 @@
-import cors from 'cors'
-import express from 'express'
+import express, { json } from 'express'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import baibulo from 'baibulo'
 import 'reflect-metadata' // required for typegraphql
 import { graphql } from './graphql'
+import authentication from './authentication';
 import { logger } from './logger'
 
 const { NODE_ENV, PORT = 3000 } = process.env
@@ -12,8 +12,10 @@ const { NODE_ENV, PORT = 3000 } = process.env
 async function main () {
   const app = express()
   app.use(helmet())
-  app.use(cors())
   app.use(cookieParser())
+  app.use(json())
+  app.use(express.static('public'))
+  app.use(authentication);
 
   const server = await graphql
   server.applyMiddleware({ app, path: '/graphql' })
