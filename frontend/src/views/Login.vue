09bb8cb77716dc71569ba3437221229f4255d664
Michał Parkitny <kontakt@michalparkitny.com>
2019-09-20 16:12:45 +0200

    [SSPAR-2] Login page UI

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,19 +1,55 @@
 <template>
-  <div class="login">
-    <LoginForm id="login" />
+  <div class="login-page container">
+    <section class="login-page__avatar">
+      <Avatar />
+    </section>
+
+    <section class="login-page__form">
+      <LoginForm id="login" />
+    </section>
+
+    <router-link class="login-page__forgot-password-link" to="/forgot">Forgot password?</router-link>
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
-import LoginForm from '@/components/LoginForm'
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import LoginForm from "@/components/LoginForm";
+import Avatar from "@/components/Avatar";
 
 @Component({
   components: {
-    LoginForm
+    LoginForm,
+    Avatar
   }
 })
-export default class Login extends Vue {
-}
+export default class Login extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+
+.login-page {
+  display: flex;
+  height: calc(100vh - $header-height);
+  flex-direction: column;
+
+  &__forgot-password-link {
+    text-align: center;
+  }
+
+  &__avatar {
+    margin-top: 72px;
+    margin-bottom: 102px;
+  }
+
+  &__form {
+    width: 100%;
+    align-self: flex-end;
+    border-bottom: 1px solid $gray;
+    padding-bottom: 16px;
+    margin-bottom: 16px;
+  }
+}
+</style>
