fab49685df45319e667e19c83dc8242262821669
wtart-softserve <wtart@softserveinc.com>
2019-09-20 14:38:45 +0200

    SSPAR-2 Added sessions table. Added functionality to create JWT.

--- backend/app/authentication/authenticate.ts
+++ backend/app/authentication/authenticate.ts
@@ -1,32 +1,35 @@
 import { Response, Request } from 'express';
 import { FieldPacket, RowDataPacket } from 'mysql'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
 import { db } from '../db'
 import { logger } from '../logger'
 
 export class Authenticator {
     authenticate = async (req: Request, res: Response) => {
         try {
-            const password = await this.getPassword(req.body.username)
-            await this.assertPasswordsMatching(req.body.password, password)
-            res.send();
+            const user = await this.getUser(req.body.username)
+            await this.assertPasswordsMatching(req.body.password, user.password)
+            const token = await this.getJSONToken(user.id, user.email);
+            await this.saveSession(token);
+            res.send(token);
         } catch (e) {
             logger.error(e)
             res.status(e.status || 500).send()
         }
     }
 
-    private async getPassword(username: string) {
+    private async getUser(username: string) {
         const [rows]: [RowDataPacket[], FieldPacket[]] = await db.execute(
           `
-          SELECT password
+          SELECT *
           FROM user
           WHERE email = ?
           `,
           [username]
         )
         this.assertUserIsFound(rows[0])
-        return rows[0].password
+        return rows[0]
     }
 
     private assertUserIsFound(user) {
@@ -36,6 +39,17 @@ export class Authenticator {
     private async assertPasswordsMatching(password, hash) {
         const match = await bcrypt.compare(password, hash)
         if (!match) throw new UnauthenticatedError('Incorrect password.')
+    }
+
+    private async getJSONToken(userID, email) {
+        return jwt.sign({userID, email}, 'ndlu35nap@nd#!nd1k8cy');
+    }
+
+    private async saveSession(token) {
+        db.execute(
+            `INSERT INTO sessions (token) VALUES (?)`,
+            [token]
+          )
     }
 }
 
