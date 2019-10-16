6d15b83c23e343334aab53f95b80f58a3d316329
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-10-16 13:24:59 +0200

    SSPAR-50 Added text to the FAQ page

--- frontend/src/views/Faq.vue
+++ frontend/src/views/Faq.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="faq-page">
-    <h1>FAQ</h1>
+    <h5>FAQ</h5>
+    <h5 class="faq-page__header">1. How does Parker work?</h5>
+    <p class="faq-page__paragraph">
+      do user actually care how much parking space is available? probably not
+      as long as he can park.
+    </p>
+    <h5 class="faq-page__header">2. Can I enroll for more than one parking date?</h5>
+    <p class="faq-page__paragraph">
+      Integer ut ultrices nunc. Maecenas quis molestie dolor.
+      Phasellus id leo lacinia nunc rutrum sollicitudin ut a justo.
+    </p>
+    <h5 class="faq-page__header">3. How much parking space is available?</h5>
+    <p class="faq-page__paragraph">
+      Integer ut ultrices nunc. Maecenas quis molestie dolor.
+      Phasellus id leo lacinia nunc rutrum sollicitudin ut a justo.
+    </p>
+    <h5 class="faq-page__header">3. How much parking space is available?</h5>
+    <p class="faq-page__paragraph">
+      Integer ut ultrices nunc. Maecenas quis molestie dolor.
+      Phasellus id leo lacinia nunc rutrum sollicitudin ut a justo.
+    </p>
   </div>
 </template>
 
@@ -14,3 +34,18 @@ import { Vue, Component } from 'vue-property-decorator'
 export default class Faq extends Vue {
 }
 </script>
+
+<style lang="scss">
+.faq-page {
+  margin: 12px;
+  font-size: 13px;
+
+  &__header {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  &__paragraph {
+  }
+}
+</style>
