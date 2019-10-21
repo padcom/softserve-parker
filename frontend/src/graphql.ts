bb98353d57e61c3f7868f546ab33e3c454b87739
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-10-21 09:33:55 +0200

    SSPAR-23 Code cleanup and added interceptors to Axios to see when a request starts and when it ends (needed later on to display progress in the user interface)

--- frontend/src/graphql.ts
+++ frontend/src/graphql.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import bus from './bus'
 
 export async function query (query: string, variables: object = {}) {
   const store = require('@/store').default
@@ -6,16 +7,13 @@ export async function query (query: string, variables: object = {}) {
     'Authorization': `Bearer ${store.state.auth.token} `
   }
 
-  return axios
-    .post('/graphql', { query, variables }, { headers })
-    .then(response => {
-      if (!response.data) {
-        throw new Error('Unable to get response data - bailing out')
-      } else if (response.data.errors) {
-        const messages = response.data.errors.map((error: any) => JSON.stringify(error.message))
-        throw new Error(messages.join('\n'))
-      } else {
-        return response.data.data
-      }
-    })
+  const response = await axios.post('/graphql', { query, variables }, { headers })
+  if (!response.data) {
+    throw new Error('Unable to get response data - bailing out')
+  } else if (response.data.errors) {
+    const messages = response.data.errors.map((error: any) => JSON.stringify(error.message))
+    throw new Error(messages.join('\n'))
+  } else {
+    return response.data.data
+  }
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './axios'
 
 Vue.config.productionTip = false
 
