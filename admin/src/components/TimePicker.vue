c84bc78d20d11bb37a61cbbf065d53fb34c76e26
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-10-21 12:18:51 +0200

    SSPAR-57 Added controls to edit system settings

---
+++ admin/src/components/TimePicker.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-menu ref="dropdown" v-model="dropdown">
+    <template #activator="{ on }">
+      <v-text-field readonly :value="value" :label="label" v-on="on" />
+    </template>
+    <v-time-picker v-if="dropdown" :value="value" @input="update" />
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class TimePicker extends Vue {
+  @Prop({ type: String, required: true }) value?: string
+  @Prop({ type: String, default: 'Label' }) label?: string
+
+  dropdown = false
+
+  update (newValue: string) {
+    this.$emit('input', newValue)
+  }
+}
+</script>
--- admin/src/views/Settings.vue
+++ admin/src/views/Settings.vue
@@ -1,13 +1,53 @@
 <template>
-  <div class="settings">
-    <h1>Settings!</h1>
-  </div>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        Settings
+      </v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="submit">
+          <v-text-field type="number"
+            v-model.number="numberOfParkingSpots"
+            label="Number of parking spots" />
+          <TimePicker
+            v-model="deadlineHour"
+            label="Time when system will assign parking space to Drivers" />
+          <TimePicker
+            v-model="cancelHour"
+            label="Time when system closes the window for opportunity to cancel reservation and taking cancelled parking reservation" />
+          <v-text-field type="number"
+            v-model.number="daysForCalclation"
+            label="The period of time that is taken into account when calculating the ranking" />
+          <v-text-field type="number"
+            v-model.number="daysForRequests"
+            label="The period of time that is allowed to request dates up front" />
+          <input type="submit" style="display: none">
+          <v-btn @click="submit">Save settings</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 
-@Component
+import TimePicker from '@/components/TimePicker.vue'
+
+@Component({
+  components: {
+    TimePicker
+  }
+})
 export default class Settings extends Vue {
+  numberOfParkingSpots = 50
+  deadlineHour = '18:00'
+  cancelHour = '07:00'
+  daysForCalclation = 90
+  daysForRequests = 30
+
+  submit () {
+    console.log('Saving settings..')
+  }
 }
 </script>
