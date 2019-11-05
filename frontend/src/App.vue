402948b28b3b39af475353cccbe4bf44169fed9f
Łukasz <lmord@softserveinc.com>
2019-11-05 14:16:45 +0100

    SSPAR-61 fix FAQ styles, scss cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -44,39 +44,6 @@ export default class App extends Vue {
 <style lang="scss">
 @import './styles';
 
-* {
-  box-sizing: border-box;
-}
-
-body {
-  &:before {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(0, 0, 0, .2);
-    z-index: 2;
-  }
-
-  &.no-scroll {
-    overflow: hidden;
-
-    &:before {
-      content: '';
-    }
-  }
-}
-
-a,
-button {
-  cursor: pointer;
-
-  &:disabled {
-    cursor: not-allowed;
-  }
-}
-
 #app {
   position: relative;
 
--- frontend/src/views/Faq.vue
+++ frontend/src/views/Faq.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="faq-page">
-    <h5>FAQ</h5>
+    <h5 class="faq-page__head">FAQ</h5>
     <h5 class="faq-page__header">1. How does Parker work?</h5>
     <p class="faq-page__paragraph">
       do user actually care how much parking space is available? probably not
@@ -35,13 +35,25 @@ export default class Faq extends Vue {}
 </script>
 
 <style lang="scss">
+@import '../styles/variables';
+
 .faq-page {
   margin: 15px;
-  font-size: 13px;
+  font-size: 18px;
+  font-family: $font-family-proxima-nova;
+
+  &__head {
+    font-weight: $font-weight-extra-bold;
+  }
 
   &__header {
     margin-top: 0;
     margin-bottom: 0;
+  }
+
+  &__paragraph {
+    font-family: $font-family-open-sans;
+    line-height: 30px;
   }
 }
 </style>
