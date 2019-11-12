ae520eea38e7a1378c8b878edeb803ccbe8f6122
Łukasz <lmord@softserveinc.com>
2019-11-12 14:30:11 +0100

    SSPAR-83 render user data at admin panel

--- admin/src/views/ParkingStatus.vue
+++ admin/src/views/ParkingStatus.vue
@@ -16,8 +16,20 @@
         :items="drivers"
         :search="search"
         disable-pagination
-        hide-default-footer />
+        hide-default-footer
+      >
+        <template v-slot:item.phone="{ item }">
+          {{ getValue(item.phone) }}
+        </template>
+        <template v-slot:item.role="{ item }">
+          {{ getValue(item.role) }}
+        </template>
+        <template v-slot:item.state="{ item }">
+          {{ getValue(item.state) }}
+        </template>
+      </v-data-table>
     </v-card>
+    <Information ref="info" />
   </v-container>
 </template>
 
@@ -26,17 +38,22 @@ import moment from 'moment'
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import { User } from '@/domain/User'
+import Information from '@/components/Information.vue'
 
-@Component
+@Component({
+  components: {
+    Information
+  }
+})
 export default class ParkingStatus extends Vue {
   headers = [
     { text: 'First Name', align: 'left', sortable: true, value: 'firstName' },
-    { text: 'Last Name', align: 'left', sortable: true, value: 'lasttName' },
+    { text: 'Last Name', align: 'left', sortable: true, value: 'lastName' },
     { text: 'Phone number', align: 'left', sortable: true, value: 'phone' },
     { text: 'Plate number', align: 'left', sortable: true, value: 'plate' },
     { text: 'Role', align: 'left', value: 'role' },
     { text: 'State', align: 'left', value: 'state' },
-    { text: 'Ranking', align: 'left', value: 'ranking' }
+    { text: 'Ranking', align: 'left', value: 'rank' }
   ]
 
   drivers = []
@@ -45,15 +62,21 @@ export default class ParkingStatus extends Vue {
 
   async mounted () {
     try {
-      const users = await User.getAll()
-      this.drivers = users
+      const drivers = await User.getAll()
+      this.drivers = drivers
     } catch (e) {
       this.drivers = []
+      // @ts-ignore
+      this.$refs.info.showError(e.message as string)
     }
   }
 
   get date () {
     return moment(new Date()).format('YYYY-MM-DD')
+  }
+
+  getValue (data: string): string {
+    return !data ? '-' : data
   }
 }
 </script>
