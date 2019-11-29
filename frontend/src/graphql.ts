632f59998ab92bebe8fd81be74c62a0c74df054c
Lukasz Mordasiewicz <lmord@softserveinc.com>
2019-11-29 15:06:08 +0100

    SSPAR-98 logout user without token

--- frontend/src/graphql.ts
+++ frontend/src/graphql.ts
@@ -1,19 +1,25 @@
 import axios from 'axios'
-import bus from './bus'
 
 export async function query (query: string, variables: object = {}) {
-  const store = require('@/store').default
-  const headers = {
-    'Authorization': `Bearer ${store.state.auth.token} `
-  }
+  const router = require('@/router').default
+  const token = window.localStorage.getItem('parker:token')
 
-  const response = await axios.post('/graphql', { query, variables }, { headers })
-  if (!response.data) {
-    throw new Error('Unable to get response data - bailing out')
-  } else if (response.data.errors) {
-    const messages = response.data.errors.map((error: any) => JSON.stringify(error.message))
-    throw new Error(messages.join('\n'))
+  if (!token) {
+    await router.push('/login')
+    throw new Error('User unauthenticated')
   } else {
-    return response.data.data
+    const headers = {
+      'Authorization': `Bearer ${token} `
+    }
+
+    const response = await axios.post('/graphql', { query, variables }, { headers })
+    if (!response.data) {
+      throw new Error('Unable to get response data - bailing out')
+    } else if (response.data.errors) {
+      const messages = response.data.errors.map((error: any) => JSON.stringify(error.message))
+      throw new Error(messages.join('\n'))
+    } else {
+      return response.data.data
+    }
   }
 }
