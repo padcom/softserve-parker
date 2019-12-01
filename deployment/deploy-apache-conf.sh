c68e64c123468445cf79988c24bb4e9487e0a417
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-12-02 00:07:34 +0100

    Removed old unused files

--- deployment/deploy-apache-conf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-echo "================================================================"
-echo "Deploying Apache configuration..."
-echo "================================================================"
-sudo cp apache/001-parker.conf /etc/apache2/sites-available/
-sudo a2ensite 001-parker.conf
-sudo service apache2 reload
