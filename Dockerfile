1f22a716b0fa087bf159a0fceb559e2429e15371
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2019-12-05 21:30:35 +0100

    Provided default values for expected environment variables - that should have been done a long time agogit diff!

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,8 @@ FROM parker-runtime
 
 MAINTAINER Matthias Hryniszak <padcom@gmail.com>
 
-ARG port=3000
 ARG domain=parker-qa.aplaline.com
 
-ENV PORT=$port
-ENV SMTP_PORT=2525
-ENV SMTP_HOST=smtp.mailtrap.io
-ENV SMTP_USER=e7a1a548c2b5fa
-ENV SMTP_PASSWORD=375ad49c0a886c
-ENV EMAIL=ssparkertesting@gmail.com
 ENV CONFIRM_URL_BASE=http://$domain
 
 COPY backend/dist /app/
--- backend/app/config/index.ts
+++ backend/app/config/index.ts
@@ -7,13 +7,13 @@ console.error( process.env)
 const { str, url, port } = envalid;
 
 const envVariableValidators = {
-  PORT: port({ desc: 'Port on which application is running'}),
-  SMTP_PORT: port({ desc: 'Port on which emails will be tunneled to SMTP server.' }),
-  SMTP_HOST: str({ desc: 'Hostname of SMTP server.' }),
-  EMAIL: str({ desc: 'The e-mail account login for the e-mail sender in this project' }),
-  CONFIRM_URL_BASE: url({ desc: 'Url of application' }),
-  SMTP_USER: str({ desc: 'User for smtp login'}),
-  SMTP_PASSWORD: str({ desc: 'The password for the smtp login' })
+  PORT: port({ default: 3000, desc: 'Port on which application is running'}),
+  SMTP_PORT: port({ default: 2525, desc: 'Port on which emails will be tunneled to SMTP server.' }),
+  SMTP_HOST: str({ default: 'smtp.mailtrap.io', desc: 'Hostname of SMTP server.' }),
+  EMAIL: str({ default: 'ssparkertesting@gmail.com', desc: 'The e-mail account login for the e-mail sender in this project' }),
+  CONFIRM_URL_BASE: url({ default: 'http://localhost:8080', desc: 'Url of application' }),
+  SMTP_USER: str({ default: 'e7a1a548c2b5fa', desc: 'User for smtp login'}),
+  SMTP_PASSWORD: str({ default: '375ad49c0a886c', desc: 'The password for the smtp login' })
 }
 
 dotenv.config()
