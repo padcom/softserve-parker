2241288421955e5487c5f57b6650c591fa32a239
Maciej Hryniszak <maciej.hryniszak@consultant.lego.com>
2020-01-29 10:59:33 +0100

    Fixed wrong specification of weekend - how I missed that is beyond my imagination

--- common/date/isWeekendDay/index.js
+++ common/date/isWeekendDay/index.js
@@ -1,5 +1,5 @@
 const dayOfWeek = require('../dayOfWeek')
 
 module.exports = function isWeekendDay (date) {
-  return [ 5, 6 ].includes(dayOfWeek(date))
+  return [ 0, 6 ].includes(dayOfWeek(date))
 }
